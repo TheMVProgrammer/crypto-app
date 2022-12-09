3606b35440c678393846e2eda25ee39310ad2ecd
TheMVProgrammer <franklin0415@hotmail.com>
2022-12-09 13:25:55 -0400

    Adding components and styling the navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import 'antd/dist/antd.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
