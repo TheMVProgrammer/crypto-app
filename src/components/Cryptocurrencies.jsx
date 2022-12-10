a6471f089deb3a834655fb989ed6ca0d20ad89ef
TheMVProgrammer <franklin0415@hotmail.com>
2022-12-10 02:37:54 -0400

    Fixing some bugs and starting CryptoDetails

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -28,13 +28,22 @@ const Cryptocurrencies = ({ simplified }) => {
     <>
       {!simplified && (
         <div className='search-crypto'>
-          <Input placeholder='Search cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)}/>
+          <Input 
+            placeholder='Search cryptocurrency' 
+            onChange={(e) => setSearchTerm(e.target.value)}              
+            />
         </div>
         )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map(currency => (
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col 
+            xs={24} 
+            sm={12} 
+            lg={6} 
+            className='crypto-card' 
+            key={currency.uuid} 
+            >
+            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
               <Card 
                 title={`${currency.rank}. ${currency.name}`}
                 extra={<img className="crypto-image" src={currency.iconUrl} alt='currency icon'/>}
